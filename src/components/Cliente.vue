89597e41489dd4aff7d358aabd5beb89fb71e783
Chrystian Ferreira da Silva <chrysterafrz@hotmail.com>
2021-01-23 22:11:34 -0300

    Aula 08 - Reatividade
    Primeiro uso de data() na classe cliente e explicações sobre como usar código JS dentro do HTML

--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,12 +1,22 @@
 <template>
     <div id="cliente">
-        <h2>Cliente aqui!</h2>
+        <h2>{{nome}}</h2>
+        <h3>{{descricao}}</h3>
+        {{2 + 2 * 2}}
     </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            nome: "Chrystian",
+            numero: "",
+            email: "",
+            idade: 0,
+            descricao: "Lorem ipsum"
+        }
+    }
 }
 </script>
 
