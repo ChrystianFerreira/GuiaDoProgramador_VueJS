fe4425f0bf0f7b9160c4b2ba6d3e4a75ddc425c9
Chrystian Ferreira da Silva <chrysterafrz@hotmail.com>
2021-02-11 20:09:33 -0300

    Finalização do projeto Pokedex - WIP

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <div class='column is-half is-offset-one-quarter'>
-      <div v-for="(pokemon, index) in pokemons" :key="index">
+  <div class='column is-half is-offset-one-quarter' id="app">
+    <h1>Pokedex</h1>
+    <input id="pokemonSearchInput" class="input is-rounded" type="text" placeholder="Busque um Pokemon" v-model="busca">
+    <button class="button is-rounded is-small is-fullwidth mt-1" @click=buscar >Buscar</button>
+    <div >
+      <div v-for="(pokemon, index) in filteredPokemons" :key="index">
         <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index + 1"/>
       </div>
     </div>
@@ -16,17 +19,38 @@ export default {
   name: "App",
   data() {
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created: function () {
     console.log("Rodando axios.get - Dentro de created: function()")
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res => {
       this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
     });
   },
   components: {
     Pokemon
+  },
+  methods: {
+    buscar: function() {
+      if(this.busca == '' || this.busca == ' '){
+        this.filteredPokemons = this.pokemons;
+      } else {
+         this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca.toLowerCase())
+      }
+    }
+  },
+  computed: {
+  //  resultadoBusca: function(){
+  //    if(this.busca == '' || this.busca == ' '){
+  //      return this.pokemons;
+  //    } else {
+  //      return this.pokemons.filter(pokemon => pokemon.name == this.busca.toLowerCase())
+  //    }
+  //  }
   }
 };
 </script>
